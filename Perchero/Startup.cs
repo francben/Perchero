8ec07fe0e63c85b96b9ab5e8cf2922271c9d2afb
Franc <franczore@gmail.com>
2017-01-06 20:34:35 -0300

    Plantilla del proyecto

---
+++ Perchero/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Perchero.Startup))]
+namespace Perchero
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
